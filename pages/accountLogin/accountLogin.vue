c3b235a2b74e3f7e625d093bb788fbe3671a16ba
liyl <liyl@hsit.com.cn>
2022-10-29 00:34:32 +0800

    initial commit

---
+++ pages/accountLogin/accountLogin.vue
@@ -0,0 +1,43 @@
+<template>
+	<view>
+		<view class="logo"><image src="../../static/studyCatAccount.svg" mode="widthFix"></image></view>
+		<view class="accountLogin-container">
+			<input type="text" placeholder="账号" v-model="account" class="accountLogin-account" />
+			<input type="password" placeholder="密码" v-model="password" class="accountLogin-password" />
+			<button class="accountLogin-btn" @click="login()">登录</button>
+		</view>
+		<view class="another-login">
+			<text class="anthor-login-text">其他登录方式</text>
+			<image src="../../static/wechat.svg" class="login-logo" mode="widthFix" @click="wechatLogin()"></image>
+			<image src="../../static/QQ .svg" class="login-logo" mode="widthFix" ></image>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			account: '',
+			password: ''
+		};
+	},
+	methods: {
+		login(){
+			uni.redirectTo({
+				
+			})
+		},
+		wechatLogin(){
+			console.log('ss')
+			uni.redirectTo({
+				url:'/pages/login/login'
+			})
+		}
+	}
+};
+</script>
+
+<style lang="less">
+@import url('accountLogin.less');
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<view class="logo"><image src="../../static/studyCat.svg" mode="widthFix"></image></view>
+		<button class="login-btn" @click="login()">
+			<image mode="widthFix" src="../../static/wx.svg"></image>
+			微信登录
+		</button>
+		<view class="register"><text class="register-link" @click="accountLogin()">账号登录</text></view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {
+		accountLogin() {
+			console.log("sss");
+			uni.redirectTo({
+				url:'/pages/accountLogin/accountLogin'
+			})
+		}
+	}
+};
+</script>
+
+<style lang="less">
+@import url(login.less);
+</style>
